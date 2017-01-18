ded3f7ee803a43447e6a760d44a95f9ece0e7012
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2017-01-18 16:00:40 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
 
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key){
+    if(key.which == 71){
+      alert('g was pressed.')
+      return
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+    return
+  })
+}
+
+$(document).ready(function(){
+  getIt(),
+  frameIt(),
+  pressIt(),
+  submitIt()
 // call functions here
 
 });
